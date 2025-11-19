ae53bbdb511b2c885366ac1d9668d300dc6d50f5
Shahzod0079 <toshmatovshahzod69@gmail.com>
2025-11-19 21:13:24 +0500

    Создание пользовательского элемента. Тошматов

---
+++ Elements/Element.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace Book_Toshmatov.Elements
+{
+    public partial class Element : UserControl
+    {
+        public Element(Classes.Book book)
+        {
+            InitializeComponent();
+
+            tbName.Text = $"Наименование: {book.Name} ({book.Year} г.)";
+
+            tbGenre.Text = $"Жанр: {book.ToGenres()}";
+
+            tbAuthor.Text = $"Автор: {book.ToAuthors()}";
+        }
+    }
+}

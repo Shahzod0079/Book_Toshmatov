c2328e73ea497045c78d2917bedcd8d3c13378c8
Shahzod0079 <toshmatovshahzod69@gmail.com>
2025-11-19 21:27:24 +0500

    Создание интерфейса MainWindow. Тошматов

--- Elements/Element.cs
+++ Elements/Element.cs
@@ -1,17 +1,16 @@
 ﻿using System.Windows.Controls;
+using Book_Toshmatov.Classes;
 
 namespace Book_Toshmatov.Elements
 {
     public partial class Element : UserControl
     {
-        public Element(Classes.Book book)
+        public Element(Book book)
         {
             InitializeComponent();
 
             tbName.Text = $"Наименование: {book.Name} ({book.Year} г.)";
-
             tbGenre.Text = $"Жанр: {book.ToGenres()}";
-
             tbAuthor.Text = $"Автор: {book.ToAuthors()}";
         }
     }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Book_Toshmatov
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Search_Book(object sender, KeyEventArgs e)
+        {
+            // Код поиска
+        }
+
+        private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Код обработки изменения выбора
         }
     }
 }

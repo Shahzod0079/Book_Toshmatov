d7d48ee5db05c731fcb76b2864e0c743e48a8f9e
Shahzod0079 <toshmatovshahzod69@gmail.com>
2025-11-19 20:35:25 +0500

    Создание класса Author. Тошматов

---
+++ Classes/Author.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Class1
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string FIO { get; set; }
+
+        public Author(int id, string fio)
+        {
+            Id = id;
+            FIO = fio;
+        }
+
+        public static List<Author> AllAuthors()
+        {
+            return new List<Author>
+            {
+                new Author(1, "Виктор Пелевин"),
+                new Author(2, "Александра Маринина"),
+                new Author(3, "Ольга Герр")
+            };
+        }
+    }
+}

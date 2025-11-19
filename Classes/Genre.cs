19e70f2973e72e492f7a2a4ba6eebf6bac6ccb6e
Shahzod0079 <toshmatovshahzod69@gmail.com>
2025-11-19 20:33:12 +0500

    Создание класса Genre. Тошматов

---
+++ Classes/Genre.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Toshmatov.Classes
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Genre(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public static List<Genre> AllGenres()
+        {
+            return new List<Genre>
+            {
+                new Genre(1, "Современная русская литература"),
+                new Genre(2, "Современные детективы"),
+                new Genre(3, "Любовное фэнтези"),
+                new Genre(4, "Попаданцы"),
+                new Genre(5, "Юмористическое фэнтези")
+            };
+        }
+    }
+}
